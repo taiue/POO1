2fd83f2e1474994426d065ad3573c822c955fca2
taiue <taime885@gmail.com>
2025-01-16 17:30:51 -0300

    Add project files.

---
+++ POO1/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace POO1
+{
+    struct Point
+    {
+        public double x;
+        public double y;
+
+
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+
+    }
+
+
+
+}
---
+++ POO1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO1;
+
+Console.WriteLine("Hello, World!");
+
+
+Point p;
+
+p.x = 10;
+p.y = 20;
+
+Console.WriteLine(p);
+
+p = new Point();
+Console.WriteLine(p);
