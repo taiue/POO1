63769f0f5ab014f9a8aedbb094990c31599d7b90
taiue <taime885@gmail.com>
2025-02-03 16:11:14 -0300

    utilização de params para parametros variaveis

--- Params/Calculator.cs
+++ Params/Calculator.cs
@@ -2,24 +2,8 @@
 {
     internal class Calculator
     {
-        //solução ruim
-        public static int Sum(int n1, int n2)
-        {
-            return n1 + n2;
-        }
 
-        public static int Sum(int n1, int n2, int n3)
-        {
-            return n1 + n2 + n3;
-        }
-
-        public static int Sum(int n1, int n2, int n3, int n4)
-        {
-            return n1 + n2 + n3 + n4;
-        }
-
-
-        public static int Sum(int[] numbers)
+        public static int Sum(params int[] numbers)
         {
             int sum = 0;
             for (int i = 0; i < numbers.Length; i++)
--- Params/Program.cs
+++ Params/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Params;
+
+int s1 = Calculator.Sum(2, 3);
+int s2 = Calculator.Sum(2, 4, 6);
+
+int s3 = Calculator.Sum(2, 4);
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
