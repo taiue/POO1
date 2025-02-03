f0dd2845b7880e5ddd9af299454ba902c9245044
taiue <taime885@gmail.com>
2025-02-03 16:41:34 -0300

    utilização de Ref e Out

---
+++ Reff_e_Out/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Reff_e_Out
+{
+    internal class Calculator
+    {
+        public static void Triple(ref int x) //modificador de parametro ref, faz com que o parametro seja uma Referencia para a variavel original
+        {
+            x = x * 3;
+        }
+
+
+        public static void Triple2(int origem, out int result) // parecido mas diferente 
+        {
+            result = origem * 3;
+        }
+
+
+        // nao é recomendado uso de ambos
+    }
+}
---
+++ Reff_e_Out/Program.cs
@@ -0,0 +1,7 @@
+﻿using Reff_e_Out;
+
+int a = 10;
+
+Calculator.Triple(ref a);
+
+Console.WriteLine(a);
