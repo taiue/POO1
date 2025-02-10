6721789615dc912920520cdbcdd1b096b2cf7225
taiue <taime885@gmail.com>
2025-02-10 16:11:22 -0300

    Exercicio com lista

---
+++ Exercicio_Lista/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio_Lista
+{
+    internal class Funcionario
+    {
+        Random rnd = new Random();
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string nome, double salario)
+        {
+            Id = rnd.Next(1, 10);
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public double AumentaSalario(double porcentagem)
+        {
+            double aumento = (porcentagem / 100) * Salario;
+            return Salario += aumento;
+        }
+  
+    
+        public override string ToString()
+        {
+            return $"Id: {Id} \n" +
+                   $"Nome: {Nome}\n" +
+                   $"Salario: {Salario}";
+        }
+
+    }
+}
---
+++ Exercicio_Lista/Program.cs
@@ -0,0 +1,41 @@
+﻿using Exercicio_Lista;
+
+List<Funcionario> funcionarios = new List<Funcionario>();
+
+Console.WriteLine("Quantos funcionarios serão registrados?: ");
+int quantidadeFuncionarios = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < quantidadeFuncionarios; i++)
+{
+    Console.Write("Digite o nome do funcionario: ");
+    string nome = Console.ReadLine();
+    Console.Write("Digite o salario do funcionario: ");
+    int salario = int.Parse(Console.ReadLine());
+    funcionarios.Add(new Funcionario(nome, salario));
+}
+
+foreach (var funcionario in funcionarios)
+{
+    Console.WriteLine("-----------------------");
+    Console.WriteLine(funcionario.ToString());
+}
+
+Console.WriteLine("Insira o Id do funcionario que recebera aumento");
+int id = int.Parse(Console.ReadLine());
+Console.WriteLine("Insira a porcentagem do aumento");
+double porcentagem = double.Parse(Console.ReadLine());
+
+foreach (var funcionario in funcionarios)
+{
+    if (funcionario.Id == id)
+        funcionario.AumentaSalario(porcentagem);
+    else
+        Console.WriteLine("Id não existe!");
+}
+
+foreach (var funcionario in funcionarios)
+{
+    Console.WriteLine("-----------------------");
+    Console.WriteLine(funcionario.ToString());
+}
+
